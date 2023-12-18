a375028115b204b833ebfe9e6f8d5be3db058c65
skay <skai-zhang@hotmail.com>
2023-12-18 11:39:27 +0800

    add util

---
+++ util/response.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type commonResponse struct {
+	State   bool   `json:"state"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+	Time    int64  `json:"time"`
+}
+
+func ReturnMessage(ctx *gin.Context, state bool, message string) {
+	ctx.JSON(200, commonResponse{
+		State:   state,
+		Message: message,
+		Time:    time.Now().Unix(),
+	})
+	ctx.Abort()
+}
+
+func ReturnData(ctx *gin.Context, state bool, obj any) {
+	ctx.JSON(200, commonResponse{
+		State: state,
+		Data:  obj,
+		Time:  time.Now().Unix(),
+	})
+	ctx.Abort()
+}
+
+func ReturnMessageData(ctx *gin.Context, state bool, message string, obj any) {
+	ctx.JSON(200, commonResponse{
+		State:   state,
+		Message: message,
+		Data:    obj,
+		Time:    time.Now().Unix(),
+	})
+	ctx.Abort()
+}
