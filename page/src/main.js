dd2d60e387f9c4a7c90d8d3744fe8a0a7e446e31
skay <skai-zhang@hotmail.com>
2023-12-29 15:42:33 +0800

    split project

--- page/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import router from './plugins/router'
-import './style.css'
-import App from './App.vue'
-// 导入等宽字体
-import 'vfonts/FiraCode.css'
-
-createApp(App).use(router).mount('#app')
