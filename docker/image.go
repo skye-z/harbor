eb30c8babc5f78985583576b40e3a79be67bc76b
skay <skai-zhang@hotmail.com>
2024-01-05 16:39:29 +0800

    add image api

--- docker/image.go
+++ docker/image.go
@@ -1,6 +1,13 @@
 package docker
 
-import "github.com/docker/docker/api/types"
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/docker/docker/api/types"
+	"github.com/gin-gonic/gin"
+)
 
 // 获取镜像列表
 func (d Docker) GetImageList() ([]types.ImageSummary, error) {
@@ -10,4 +17,50 @@ func (d Docker) GetImageList() ([]types.ImageSummary, error) {
 	}
 
 	return images, nil
+}
+
+// 删除镜像
+func (d Docker) Remove(id string, prune bool, force bool) error {
+	_, err := d.Session.ImageRemove(d.Context, id, types.ImageRemoveOptions{
+		PruneChildren: prune,
+		Force:         force,
+	})
+	return err
+}
+
+// 拉取镜像
+func (d Docker) Pull(ctx *gin.Context, id string, store string, name string, platform string) {
+	log.Printf("[Image] pull %s(%s) for %s\n", name, platform, store)
+	out, err := d.Session.ImagePull(d.Context, store+"/"+name, types.ImagePullOptions{
+		Platform: platform,
+	})
+	if err != nil {
+		log.Println("[Image] pull failed:", err)
+		return
+	}
+	defer out.Close()
+	ctx.Header("Content-Type", "application/octet-stream")
+	ctx.Status(http.StatusOK)
+	_, err = io.Copy(ctx.Writer, out)
+	if err != nil {
+		log.Printf("[Image] pull %s pulled failed\n", name)
+	} else {
+		log.Printf("[Image] pull %s pulled successfully\n", name)
+	}
+}
+
+// 打标签
+func (d Docker) AddTag(id string, tag string) error {
+	err := d.Session.ImageTag(d.Context, id, tag)
+	return err
+}
+
+// 获取镜像详情
+func (d Docker) GetInfo(id string) (types.ImageInspect, error) {
+	info, _, err := d.Session.ImageInspectWithRaw(d.Context, id)
+	if err != nil {
+		return types.ImageInspect{}, err
+	}
+
+	return info, nil
 }
--- service/image.go
+++ service/image.go
@@ -12,16 +12,62 @@ type ImageService struct {
 }
 
 func NewImageService(client *docker.Docker) *ImageService {
-	ds := new(ImageService)
-	ds.Client = client
-	return ds
+	is := new(ImageService)
+	is.Client = client
+	return is
 }
 
-func (ds ImageService) GetList(ctx *gin.Context) {
-	list, err := ds.Client.GetImageList()
+// 获取镜像列表
+func (is ImageService) GetList(ctx *gin.Context) {
+	list, err := is.Client.GetImageList()
 	if err != nil {
-		util.ReturnMessage(ctx, false, "获取列表失败")
+		util.ReturnMessage(ctx, false, "获取镜像列表失败")
 	} else {
 		util.ReturnData(ctx, true, list)
+	}
+}
+
+// 删除镜像
+func (is ImageService) Remove(ctx *gin.Context) {
+	id := ctx.Query("id")
+	prune := ctx.Query("prune")
+	force := ctx.Query("id")
+	err := is.Client.Remove(id, prune == "1", force == "1")
+	if err != nil {
+		util.ReturnMessage(ctx, false, "镜像删除失败")
+	} else {
+		util.ReturnMessage(ctx, true, "镜像删除成功")
+	}
+}
+
+// 拉取镜像
+func (is ImageService) Pull(ctx *gin.Context) {
+	id := ctx.Query("id")
+	store := ctx.Query("store")
+	name := ctx.Query("name")
+	platform := ctx.Query("platform")
+	is.Client.Pull(ctx, id, store, name, platform)
+}
+
+// 打标签
+func (is ImageService) AddTag(ctx *gin.Context) {
+	id := ctx.Query("id")
+	tag := ctx.Query("tag")
+	err := is.Client.AddTag(id, tag)
+	if err != nil {
+		util.ReturnMessage(ctx, false, "镜像标记失败")
+	} else {
+		util.ReturnMessage(ctx, true, "镜像标记成功")
+	}
+}
+
+// 获取镜像详情
+func (is ImageService) GetInfo(ctx *gin.Context) {
+	id := ctx.Query("id")
+	info, err := is.Client.GetInfo(id)
+	if err != nil {
+		util.ReturnMessage(ctx, false, "获取镜像详情失败")
+	} else {
+		util.ReturnData(ctx, true, info)
 	}
 }
