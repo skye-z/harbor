f09e9f51e6f4c98f67d30980b793e11a7e28d641
skay <skai-zhang@hotmail.com>
2024-01-05 16:39:40 +0800

    add host monitor api

--- route/route.go
+++ route/route.go
@@ -54,6 +54,10 @@ func (r Route) addPublicRoute() {
 		ctx.Request.URL.Path = "/app"
 		r.Router.HandleContext(ctx)
 	})
+	ms := service.NewMonintorService(r.DockerClient)
+	r.Router.GET("/api/device/info", ms.GetDeviceInfo)
+	r.Router.GET("/api/system/use", ms.GetUse)
+
 	ds := service.NewDockerService(r.DockerClient)
 	r.Router.GET("/api/docker/info", ds.GetInfo)
 
@@ -67,6 +71,7 @@ func (r Route) addPublicRoute() {
 	r.Router.GET("/api/container/kill", cs.KillContainer)
 	r.Router.GET("/api/container/pause", cs.PauseContainer)
 	r.Router.GET("/api/container/unpause", cs.UnpauseContainer)
+	r.Router.GET("/api/container/remove", cs.RemoveContainer)
 	r.Router.GET("/api/container/terminal", cs.ConnectTerminal)
 	r.Router.GET("/api/container/diff", cs.GetDiff)
 	r.Router.GET("/api/container/stat", cs.GetStat)
@@ -74,6 +79,10 @@ func (r Route) addPublicRoute() {
 
 	is := service.NewImageService(r.DockerClient)
 	r.Router.GET("/api/image/list", is.GetList)
+	r.Router.GET("/api/image/remove", is.Remove)
+	r.Router.GET("/api/image/pull", is.Pull)
+	r.Router.GET("/api/image/tag", is.AddTag)
+	r.Router.GET("/api/image/info", is.GetInfo)
 
 	ns := service.NewNetworkService(r.DockerClient)
 	r.Router.GET("/api/network/list", ns.GetList)
---
+++ service/monitor.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"harbor/docker"
+	"harbor/monitor"
+	"harbor/util"
+
+	"github.com/gin-gonic/gin"
+)
+
+type MonintorService struct {
+	Client *docker.Docker
+}
+
+func NewMonintorService(client *docker.Docker) *MonintorService {
+	ms := new(MonintorService)
+	return ms
+}
+
+// 获取系统信息
+func (ms MonintorService) GetDeviceInfo(ctx *gin.Context) {
+	info := monitor.GetDeviceInfo()
+	util.ReturnData(ctx, true, info)
+}
+
+// 获取资源占用情况
+func (ms MonintorService) GetUse(ctx *gin.Context) {
+	use := monitor.GetUse()
+	util.ReturnData(ctx, true, use)
+}
