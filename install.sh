a1e836ae32c20bbd3279849c951954b63ea41cab
skay <skai-zhang@hotmail.com>
2024-05-23 15:56:12 +0800

    update install script

--- install.sh
+++ install.sh
@@ -6,9 +6,26 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+# 检查是否安装了 Docker
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Please install Docker and try again."
+    exit 1
+fi
+
 # 设置服务名称和描述
 SERVICE_NAME="harbor"
 SERVICE_DESCRIPTION="Harbor"
+
+# 获取系统架构
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    HARBOR_FILE="harbor_linux_amd64"
+elif [ "$ARCH" = "aarch64" ]; then
+    HARBOR_FILE="harbor_linux_arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
 
 # 设置可执行文件的下载 URL 和版本
 HARBOR_VERSION=$(curl -sL https://api.github.com/repos/skye-z/harbor/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
@@ -16,28 +33,30 @@ if [ -z "$HARBOR_VERSION" ]; then
     echo "Failed to retrieve the latest Harbor version."
     exit 1
 fi
-HARBOR_DOWNLOAD_URL="https://github.com/skye-z/harbor/releases/download/${HARBOR_VERSION}/harbor_linux_amd64"
+HARBOR_DOWNLOAD_URL="https://github.com/skye-z/harbor/releases/download/${HARBOR_VERSION}/${HARBOR_FILE}"
 
 # 设置工作目录
 WORKING_DIRECTORY="/opt/harbor"
 
 # 创建 Systemd 服务单元文件
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
-echo "[Unit]" > $SERVICE_FILE
-echo "Description=${SERVICE_DESCRIPTION}" >> $SERVICE_FILE
-echo "After=docker.service" >> $SERVICE_FILE
-echo "" >> $SERVICE_FILE
-echo "[Service]" >> $SERVICE_FILE
-echo "ExecStart=${WORKING_DIRECTORY}/harbor >> /var/log/harbor.log 2>&1" >> $SERVICE_FILE
-echo "WorkingDirectory=${WORKING_DIRECTORY}" >> $SERVICE_FILE
-echo "Restart=always" >> $SERVICE_FILE
-echo "" >> $SERVICE_FILE
-echo "[Install]" >> $SERVICE_FILE
-echo "WantedBy=multi-user.target" >> $SERVICE_FILE
+cat <<EOF > $SERVICE_FILE
+[Unit]
+Description=${SERVICE_DESCRIPTION}
+After=docker.service
+
+[Service]
+ExecStart=${WORKING_DIRECTORY}/harbor >> /var/log/harbor.log 2>&1
+WorkingDirectory=${WORKING_DIRECTORY}
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 install_harbor_online() {
     # 创建工作目录
-    sudo mkdir -p $WORKING_DIRECTORY
+    mkdir -p $WORKING_DIRECTORY
 
     # 下载 Harbor 可执行文件
     curl -LJ $HARBOR_DOWNLOAD_URL -o ${WORKING_DIRECTORY}/harbor
@@ -46,65 +65,68 @@ install_harbor_online() {
     chmod +x ${WORKING_DIRECTORY}/harbor
 
     # 重载 Systemd 配置
-    sudo systemctl daemon-reload
+    systemctl daemon-reload
 
     # 启动服务
-    sudo systemctl start $SERVICE_NAME
+    systemctl start $SERVICE_NAME
 
     echo "Harbor service installed successfully!"
 }
 
 install_harbor_offline() {
     # 检查是否存在离线安装文件
-    if [ -f "harbor_linux_amd64" ]; then
+    if [ -f $HARBOR_FILE ]; then
+        # 创建工作目录
+        mkdir -p $WORKING_DIRECTORY
+
         # 复制离线安装文件到工作目录
-        cp harbor_linux_amd64 ${WORKING_DIRECTORY}/harbor
+        cp ${HARBOR_FILE} ${WORKING_DIRECTORY}/harbor
 
         # 赋予可执行权限
         chmod +x ${WORKING_DIRECTORY}/harbor
 
         # 重载 Systemd 配置
-        sudo systemctl daemon-reload
+        systemctl daemon-reload
 
         # 启动服务
-        sudo systemctl start $SERVICE_NAME
+        systemctl start $SERVICE_NAME
 
         echo "Harbor service installed successfully!"
     else
-        echo "Error: Offline installation file 'harbor_linux_amd64' not found. Please download it manually to the current directory."
+        echo "Error: Offline installation file '$HARBOR_FILE' not found. Please download it manually to the current directory."
         exit 1
     fi
 }
 
 uninstall_harbor() {
     # 停止服务
-    sudo systemctl stop $SERVICE_NAME
+    systemctl stop $SERVICE_NAME
 
     # 禁用开机自启
-    sudo systemctl disable $SERVICE_NAME
+    systemctl disable $SERVICE_NAME
 
     # 删除工作目录
-    sudo rm -rf $WORKING_DIRECTORY
+    rm -rf $WORKING_DIRECTORY
 
     # 删除 Systemd 服务文件
-    sudo rm -f $SERVICE_FILE
+    rm -f $SERVICE_FILE
 
     # 重载 Systemd 配置
-    sudo systemctl daemon-reload
+    systemctl daemon-reload
 
     echo "Harbor service uninstalled successfully!"
 }
 
 enable_autostart() {
     # 启用开机自启
-    sudo systemctl enable $SERVICE_NAME
+    systemctl enable $SERVICE_NAME
 
     echo "Autostart enabled for Harbor service!"
 }
 
 disable_autostart() {
     # 禁用开机自启
-    sudo systemctl disable $SERVICE_NAME
+    systemctl disable $SERVICE_NAME
 
     echo "Autostart disabled for Harbor service!"
 }
@@ -120,10 +142,10 @@ read -p "Enter option number: " option
 
 # 根据选项执行相应操作
 case $option in
-    1) install_harbor_online;;
-    2) install_harbor_offline;;
-    3) uninstall_harbor;;
-    4) enable_autostart;;
-    5) disable_autostart;;
-    *) echo "Invalid option";;
+    1) install_harbor_online ;;
+    2) install_harbor_offline ;;
+    3) uninstall_harbor ;;
+    4) enable_autostart ;;
+    5) disable_autostart ;;
+    *) echo "Invalid option" ;;
 esac
