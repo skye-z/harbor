affefc9b289f27c1a77ed1a93c42c8f73d261161
Skye <skai-zhang@hotmail.com>
2025-09-20 22:05:14 +0800

    feat: 搭建基本框架

---
+++ backend/internal/data/models.go
@@ -0,0 +1,11 @@
+package data
+
+import "time"
+
+// 用户模型
+type User struct {
+	ID        int       `json:"id" xorm:"pk autoincr"`  // 用户 ID，主键，自增
+	Username  string    `json:"username" xorm:"unique"` // 用户名，唯一
+	Password  string    `json:"password"`               // 密码
+	CreatedAt time.Time `json:"created_at"`             // 创建时间
+}
---
+++ backend/internal/data/source.go
@@ -0,0 +1,66 @@
+package data
+
+import (
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+	_ "modernc.org/sqlite"
+	"xorm.io/xorm"
+)
+
+func InitDB() *xorm.Engine {
+	engine, err := xorm.NewEngine("sqlite", "./harbor.db")
+	if err != nil {
+		panic(err)
+	}
+	return engine
+}
+
+func InitDBTable(engine *xorm.Engine) {
+	err := engine.Sync2(new(User))
+	if err != nil {
+		panic(err)
+	}
+
+	count, err := engine.Count(&User{})
+	if err != nil {
+		panic(err)
+	}
+	if count == 0 {
+		// 默认密码 "harbor-skye"
+		defaultPasswordMD5 := "58c823f11203f20fda6b4deb81d30b3b"
+
+		if len(defaultPasswordMD5) != 32 {
+			panic("Default password must be a 32-character MD5 hash")
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(defaultPasswordMD5), bcrypt.DefaultCost)
+		if err != nil {
+			panic(err)
+		}
+		admin := User{
+			Username:  "admin",
+			Password:  string(hashedPassword),
+			CreatedAt: time.Now(),
+		}
+		_, err = engine.Insert(&admin)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+var Engine *xorm.Engine
+
+// GetDB 获取数据库引擎
+func GetDB() *xorm.Engine {
+	return Engine
+}
+
+// Close 关闭数据库连接
+func Close() error {
+	if Engine != nil {
+		return Engine.Close()
+	}
+	return nil
+}
